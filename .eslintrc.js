2c7a02efcf552143212fb5ceaf0b1e86f5ecdeac
Roberto Langarica <roberto.langarica@gmail.com>
2021-05-03 17:00:50 -0500

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,20 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard',
-    '@vue/typescript/recommended'
+    '@vue/typescript'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-unused-components':'warn',
+    'prefer-const':'warn',
+    '@typescript-eslint/interface-name-prefix':'off',
+    'no-unused-vars':'warn'
   },
   overrides: [
     {
