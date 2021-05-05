193b66c0049a185e4fd8f128bf2c130641dcc892
Roberto Langarica <roberto.langarica@gmail.com>
2021-05-04 22:29:40 -0500

    src/store/index.ts: Modified to be only a bypass for the store and modules exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
-import { createStore } from 'vuex'
+import { store, IGlobalState } from './store'
+import { useCollection } from './basic_collection'
+import { InjectionKey } from 'vue'
+import { Store, useStore as baseUseStore } from 'vuex'
+import { useActivities } from './activities/activities'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export const key: InjectionKey<Store<IGlobalState>> = Symbol()
+
+export function useStore () {
+  return baseUseStore(key)
+}
+
+export { useCollection, useActivities }
+export default store
