2b44d34ff0b6c6f141f9de25d8e20d0482c29f77
Roberto Langarica <roberto.langarica@gmail.com>
2021-05-04 22:27:53 -0500

    src/quasar-user-options.js: Adding Notify and Dialog to the quasar plugins

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -3,11 +3,14 @@ import './styles/quasar.scss'
 import iconSet from 'quasar/icon-set/fontawesome-v5.js'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Notify, Dialog } from 'quasar'
 
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
   plugins: {
+    Notify,
+    Dialog
   },
   iconSet: iconSet
 }
